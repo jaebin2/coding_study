57ce08e728694f253559135b90586aa67fe4ab50
jaebin2 <90362685+jaebin2@users.noreply.github.com>
2022-04-28 20:26:51 +0900

    coding

--- Leet_1823.py
+++ Leet_1823.py
@@ -2,12 +2,10 @@ class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         tmp = list(range(n))
         idx = 0
-
         while len(tmp) != 1:
             idx += k - 1
-            while idx >= len(tmp):
-                idx -= len(tmp)
-
+            if idx >= len(tmp):
+                idx %= len(tmp)
             del(tmp[idx])
             
         return tmp[0] + 1

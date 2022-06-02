e94542499a815492e7200d1f6fb281d9a558c62e
jaebin2 <90362685+jaebin2@users.noreply.github.com>
2022-06-02 17:29:07 +0900

    coding

--- Leet_1791.py
+++ Leet_1791.py
@@ -1,8 +1,8 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
         tmp1, tmp2 = edges[0]
-        for tmp3, tmp4 in edges[1:]:
-            if (tmp3 == tmp1) | (tmp3 == tmp2):
-                return tmp3
-            if (tmp4 == tmp1) | (tmp4 == tmp2):
-                return tmp4
+        tmp3, tmp4 = edges[1]
+        if (tmp3 == tmp1) | (tmp3 == tmp2):
+            return tmp3
+        if (tmp4 == tmp1) | (tmp4 == tmp2):
+            return tmp4
